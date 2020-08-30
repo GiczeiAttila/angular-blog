abc144946757abfea16dd1bc72535b5a41585bc2
Papp Niki <p.niki666@gmail.com>
2020-08-30 10:43:01 +0200

    add displayName to PostCategories

--- src/main/java/com/progmasters/reactblog/domain/PostCategories.java
+++ src/main/java/com/progmasters/reactblog/domain/PostCategories.java
@@ -2,7 +2,17 @@ package com.progmasters.reactblog.domain;
 
 public enum PostCategories {
 
-    CORPORATE,
-    FREE_TIME_ACTIVITIES,
-    PROFESSIONAL;
+    CORPORATE("Corporate news"),
+    FREE_TIME_ACTIVITIES("Free time activities"),
+    PROFESSIONAL("Professional news");
+
+    private String displayName;
+
+    PostCategories(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 }
