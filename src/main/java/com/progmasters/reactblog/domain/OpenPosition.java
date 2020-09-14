a93824321c5d864ec3ea85699e4a4b86972a67ab
Giczei Attila <giczei.attila01@gmail.com>
2020-09-14 18:59:40 +0200

    created OpenPosition, OpenPositionFormDto, ApplicantForOpenPosition

---
+++ src/main/java/com/progmasters/reactblog/domain/OpenPosition.java
@@ -0,0 +1,83 @@
+package com.progmasters.reactblog.domain;
+
+import com.progmasters.reactblog.domain.dto.OpenPositionFormDto;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class OpenPosition {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String positionName;
+    private String description;
+    private LocalDate deadline;
+
+    @OneToMany(mappedBy = "openPosition")
+    private List<ApplicantForOpenPosition> applicants;
+
+    @ManyToOne
+    private User user;
+
+    public OpenPosition() {
+    }
+
+    public OpenPosition(OpenPositionFormDto openPositionFormDto) {
+        this.positionName = openPositionFormDto.getPositionName();
+        this.description = openPositionFormDto.getDescription();
+        this.deadline = LocalDate.parse(openPositionFormDto.getDeadline());
+        this.applicants = new ArrayList<>();
+        //this.recommendedPeoplesCVList = new ArrayList<>();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPositionName() {
+        return positionName;
+    }
+
+    public void setPositionName(String positionName) {
+        this.positionName = positionName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(LocalDate deadline) {
+        this.deadline = deadline;
+    }
+
+    public List<ApplicantForOpenPosition> getApplicants() {
+        return applicants;
+    }
+
+    public void setApplicants(List<ApplicantForOpenPosition> applicants) {
+        this.applicants = applicants;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
