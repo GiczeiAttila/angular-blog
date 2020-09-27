fda7fb87f827d359e4395c1fb81f4ae24f3a5994
Magyar Attila <magyar.attila317@gmail.com>
2020-09-27 12:45:43 +0200

    add code-review

--- src/main/java/com/progmasters/reactblog/AngularBlogApplication.java
+++ src/main/java/com/progmasters/reactblog/AngularBlogApplication.java
@@ -9,6 +9,14 @@ public class AngularBlogApplication {
     //TODO Review - Mivel kezd eléggé dagadni az alkalmazás, el lehetne gondolkozni a
     // package-by-feature elrendezésben ( google: package-by-feature Vs. package-by-layer)
 
+    //TODO Review - Érdemes mindenkinek átfutni a code style beállításokat,
+    // és ott tweakelni kicsit a beállítást, esetleg megegyezni valami közös configban,
+    // Majd ezt commit előtt mindig futtatgatni
+    // ( CTRL + ALT + L -t ki lehet adni akár egész packagekre is)
+    // Brutál sok hasznos opció van, csoportosítja a getter/settereket, methodokat, fieldeket,
+    // javítja az indentálást, kitörli ha 1nél több üres sor van egymás után stb. stb.
+    // Ezekkel sokat tudnak segíteni, hogy a kód formailag egységes legyen projekt szinten
+    // Ha egy valaki megcsinálja a konfigot, az utánna ezt tudja exportálni/megosztani a többiekkel!
     public static void main(String[] args) {
         SpringApplication.run(AngularBlogApplication.class, args);
     }
